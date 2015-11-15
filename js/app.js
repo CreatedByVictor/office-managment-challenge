2754c04ef0a6aa69156fdaab5f01ab853b2b5254
Victor Lawrence <lawrence_victor@columbusstate.edu>
2015-11-15 07:36:34 -0500

    Thinking...

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 (function(){
 	var app = angular.module('OrganizationApp',['ui-router']);
 	//console.log("[app.js loaded.]");
+	app.factory("DataFactory", function('$scope'){
+		function getProjects(sort){
+			
+		};
+		
+
+		return {
+			"getProjects":getProjects
+		};
+	});
 })()
